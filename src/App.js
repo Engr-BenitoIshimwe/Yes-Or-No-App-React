cf47aadf8849921707fdcd97d7c0e14d77d7c556
Benito Ishimwe <benishimwe31@gmail.com>
2022-05-02 21:54:11 +0200

    Yes Or No App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Ternary from './Ternary';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <Ternary />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Ternary.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Ternary() {
+  const [isGoingOut, setIsGoingOut] = React.useState(true);
+
+  function changeMind() {
+    setIsGoingOut((prevState) => !prevState);
+  }
+  return (
+    <div className='state'>
+      <h1 className='state--title'>Do I feel like going out tonight?</h1>
+      <div onClick={changeMind} className='state--value'>
+        <h1>{isGoingOut ? 'Yes' : 'No'}</h1>
+      </div>
+    </div>
+  );
+}
